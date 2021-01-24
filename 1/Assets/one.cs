fe4a1a50e2ec3fadfffe481b15d0578ef086fba6
DESKTOP-FOQ125D\User <pla.preyaporn.17@gmail.com>
2021-01-24 23:51:52 +0700

    Send Assignment Week1

---
+++ 1/Assets/one.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class one : MonoBehaviour
+{
+    public InputField kidsa;
+    public Text Looksa;
+    private string one1, two, three, four, five;
+    private string number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        one1 = "Hi";
+        two = "Sawatdee";
+        three = "Konnichiwa";
+        four = "Annyeonghaseyo";
+        five = "nihao";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        number = kidsa.text;
+    }
+
+    public void wowsaa()
+    {
+        if (number == one1 || number == two || number == three || number == four || number == five)
+        {
+            Looksa.text = "[ " + $"<color=purple>{number}</Color>" + " ] " + " is found. ";
+        }
+        else
+        {
+            Looksa.text = "[ " + $"<color=pink>{number}</Color>" + " ] " + " is not found. ";
+        }
+    }
+
+
+
+
+
+}
